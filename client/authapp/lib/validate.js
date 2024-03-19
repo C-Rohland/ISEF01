c519588f557c1fbfc8e62d3a90f3087637fbbaae
crohland <christine.rohland@iu-study.org>
2024-03-19 12:21:17 +0100

    Create validate.js

---
+++ client/authapp/lib/validate.js
@@ -0,0 +1,57 @@
+export default function login_validate(values){
+    const errors = {};
+
+    if (!values.email) {
+        errors.email = 'Erforderlich';
+    } else if (!/^[A-Z0-9._%+-]+@(iu-study\.org|iu\.org|iubh-fernstudium\.de)$/i.test(values.email)) {
+        errors.email = 'Ungültige Emailadresse';
+    }
+
+    // validation for password
+    if(!values.password){
+        errors.password = "Erforderlich";
+    } else if(values.password.length < 8 || values.password.length > 20){
+        errors.password = "Das Passwort muss zwischen 8 und 20 Zeichen enthalten!";
+    } else if(values.password.includes(" ")){
+        errors.password = "Ungültiges Passwort";
+    }
+
+    return errors;
+
+}
+
+export function registerValidate(values){
+    const errors = {};
+
+    if(!values.username){
+        errors.username = "Erforderlich";
+    }else if(values.username.includes(" ")){
+        errors.username = "Ungültiger Username"
+    }
+
+    if (!values.email) {
+        errors.email = 'Erforderlich';
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+        errors.email = 'Ungültige Emailadresse';
+    }
+
+       // validation for password
+       if(!values.password){
+        errors.password = "Erforderlich";
+    } else if(values.password.length < 8 || values.password.length > 20){
+        errors.password = "Das Passwort muss zwischen 8 und 20 Zeichen enthalten!";
+    } else if(values.password.includes(" ")){
+        errors.password = "Ungültiges Passwort";
+    }
+
+    // validate confirm password
+    if(!values.cpassword){
+        errors.cpassword = "Erforderlich";
+    } else if(values.password !== values.cpassword){
+        errors.cpassword = "Password Not Match...!"
+    } else if(values.cpassword.includes(" ")){
+        errors.cpassword = "Passwörter stimmen nicht überein"
+    }
+
+    return errors;
+}
