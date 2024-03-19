d3af5449152a2191c1e63124ddc4892d4736b1f2
crohland <christine.rohland@iu-study.org>
2024-03-19 12:18:04 +0100

    Login Validate

--- client/authapp/pages/login.js
+++ client/authapp/pages/login.js
@@ -7,15 +7,18 @@ import {RiAtFill, RiFileLockLine } from "react-icons/ri";
 import { useState } from 'react';
 import { signIn, signOut } from "next-auth/react"
 import { useFormik } from 'formik';
+import login_validate from '../lib/validate';
 
 export default function Login(){
 
     const [show, setShow] = useState(false)
+    //formik hook
     const formik = useFormik({
         initialValues: {
             email: '',
             password: ''
         },
+        validate : login_validate,
         onSubmit
     })
 
@@ -50,19 +53,23 @@ export default function Login(){
                 <RiAtFill size={25}/>
                 </span>
             </div>
+            {formik.errors.email && formik.touched.email ? <span className='text-rose-500'>{formik.errors.email}</span> : <></>}
+               
             <div className={styles.input_group}>
                 <input 
                 type={`${show ? "text" : "password"}`}
                 name='password'
                 placeholder='Dein Passwort'
                 className={styles.input_text}
-                {...formik.getFieldProps('email')}
+                {...formik.getFieldProps('password')}
                 />
                 <span className='icon flex items-center px-4' onClick={() => setShow(!show)}>
                 <RiFileLockLine size={25}/>
                 </span>
             </div>
 
+            {formik.errors.password && formik.touched.password ? <span className='text-rose-500'>{formik.errors.password}</span> : <></>}
+                
           {/* login button */}   
             <div className={styles.button}>
                 <button type='submit'>
