dd29ab7fd4ab600712dc8dacc8267f0b55d61a23
crohland <christine.rohland@iu-study.org>
2024-03-21 12:38:08 +0100

    Passwortsichtbarkeit und Kommentare hinzugefügt /Login

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,23 +1,33 @@
+// Importiert die benötigten Module und Komponenten
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { HiEye, HiEyeOff } from "react-icons/hi";
 
 
 function Login() {
-  // Zustände für Benutzername und Passwort
+
+  // Definiert Zustandsvariablen für die E-Mail, das Passwort und die Sichtbarkeit des Passworts
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+
+  // Nutzt den useNavigate Hook, um programmatische Navigation zu ermöglichen
   const navigate = useNavigate();
+
+  // Funktion zum Umschalten der Sichtbarkeit des Passworts
+  const toggleShowPassword = () => setShowPassword(!showPassword);
 
   // Funktion, die beim Einreichen des Formulars ausgelöst wird
   const handleSubmit = async (e) => {
     e.preventDefault(); // Verhindert das Neuladen der Seite beim Einreichen
 
-    // Einfache Validierung
+   // Einfache Validierung, um sicherzustellen, dass die Felder ausgefüllt sind
     if (!email || !password) {
       alert('Bitte füllen Sie beide Felder aus.');
       return;
     }
 
+    // Versucht, den Login mit der API durchzuführen
     try {
       const response = await fetch(`${process.env.REACT_APP_SERVER_HOSTNAME}/api/login`, {
         method: 'POST',
@@ -34,21 +44,25 @@ function Login() {
         throw new Error('Login fehlgeschlagen');
       }
 
+      // Verarbeitet die Antwort
       const data = await response.json();
       console.log('Login erfolgreich:', data);
       alert('Login erfolgreich!');
-// Hier könntest du den Token speichern
-      navigate('/main'); // Ändere '/main' zu dem Pfad, der deiner Hauptkomponente entspricht
+
+       // Navigiert zur Hauptseite
+      navigate('/main'); 
     } catch (error) {
       console.error(error);
       alert('Anmeldung fehlgeschlagen: ' + error.message);
     }
   };
 
+  //Navigiert zur Registrierungsseite
   const navigateToRegister = () => {
-    navigate('/register'); // Pfad zur Registrierungsseite
+    navigate('/register'); 
   };
 
+  //Login-Formular
   return (
     <div className="login-container">
       <div className="content-box">
@@ -69,16 +83,25 @@ function Login() {
                 placeholder="Email"
               />
             </div>
+
+
             <div className="form-group">
-              <label htmlFor="password">Passwort</label>
-              <input
-                type="password"
-                id="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                placeholder="Passwort"
-              />
-            </div>
+                <label htmlFor="password">Passwort</label>
+                <div className="input-group">
+                  <input
+                    type={showPassword ? "text" : "password"}
+                    id="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    placeholder="Passwort"
+                    required
+                  />
+                  <span onClick={toggleShowPassword} className="password-icon">
+                    {showPassword ? <HiEyeOff /> : <HiEye />}
+                  </span>
+                </div>
+              </div>
+
         <button type="submit">Anmelden</button>
         <div>
             <p>Du hast noch keinen Account? Dann registriere dich</p>
