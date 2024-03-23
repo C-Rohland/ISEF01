ec7d7cdcc08139102876386b31690f471724460d
crohland <christine.rohland@iu-study.org>
2024-03-23 08:17:07 +0100

    test

---
+++ client/src/components/CreateQuiz.js
@@ -0,0 +1,53 @@
+//import React, { useRef } from 'react'
+import { useNavigate } from 'react-router-dom';
+import '../styles/Main.css'
+
+export default function Main() {
+
+    const navigate = useNavigate();
+
+    const username = sessionStorage.getItem('username');
+
+
+
+    const startQuiz = () => {
+        navigate('/quiz'); 
+        }
+    const createQuiz = () => {
+        navigate('/createQuiz'); 
+        }
+    const seeLeaderboard = () => {
+        navigate('/leaderboard'); 
+        }
+    
+
+  return (
+    <div>
+    <div className='container'>
+            {username && <h1 className='title text-light'>Hallo {username}</h1>}
+            
+            <h2>Quiz-Regeln</h2>
+            <ul>
+                <p>Das Quiz besteht aus 10 Fragen.</p>
+                <p>Jede Frage hat 4 Antwortmöglichkeiten, wovon nur eine richtig ist.</p>
+                <p>Für jede richtige Antwort erhältst du Punkte. Die Punkte werden am Ende addiert.</p>
+                <p>Es gibt kein Zeitlimit für die Beantwortung der Fragen, aber versuche zügig zu antworten.</p>
+                <p>Am Ende des Quiz kannst du deine Gesamtpunktzahl sehen und dich mit anderen vergleichen.</p>
+            </ul>
+             <p>Viel Erfolg!</p>
+     </div>
+
+          
+     <div className='start'>
+            <button onClick={startQuiz}>Neues Quiz starten</button>
+          
+            <button onClick={seeLeaderboard}>Leaderboard</button>
+          
+            <button onClick={createQuiz}>Quiz erstellen</button>
+            </div>
+            </div>
+
+            
+  );
+
+}
