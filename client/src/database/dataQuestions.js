a13828d52d66aa72bfa78349ddf73a5b8cc385df
crohland <christine.rohland@iu-study.org>
2024-03-23 18:05:07 +0100

    Create dataQuestions.js

---
+++ client/src/database/dataQuestions.js
@@ -0,0 +1,194 @@
+const dataQuestions = [ 
+    {_id: "1a",
+      question: "Digitale Güter liegen in Form von ...",
+      options: [
+        "Binärdaten vor",
+        "Computerdaten vor",
+        "elektronischen daten vor",
+        "großen Datenmengen vor"],
+      answer: "Binärdaten vor",
+      subjectname: "Wirtschaftsinformatik"
+    },
+    {
+      _id: "2a",
+      question: "In welcher Phase findet die Migration von Daten statt?",
+      options: [
+        "In der Phase Abschaltung",
+        "In der Phase Integration",
+        "In der Phase Betrieb",
+        "In der Phase Erstellung / Entwicklung"
+      ],
+      answer: "In der Phase Abschaltung",
+      subjectname: "Wirtschaftsinformatik"
+    },
+    {
+      _id: "3a",
+      question: "Unter dem Begriff On-Demand versteht man, dass …",
+      options: [
+        "die zur Verfügungstellung innerhalb eines Tages erfolgen muss.",
+        "ein Service zu jeder Zeit, wenn man ihn benutzen möchte, in Echtzeit zur Verfügung steht.",
+        "die zur Verfügungstellung innerhalb von zwei Stunden erfolgen muss.",
+        "die zur Verfügungstellung innerhalb einer Stunde erfolgen muss"
+      ],
+      answer: "ein Service zu jeder Zeit, wenn man ihn benutzen möchte, in Echtzeit zur Verfügung steht.",
+      subjectname: "Wirtschaftsinformatik"
+    },
+    {
+      _id: "4a",
+      question: "test1 Digitale Güter liegen in Form von ...",
+      options: [
+        "Binärdaten vor",
+        "Computerdaten vor",
+        "elektronischen daten vor",
+        "großen Datenmengen vor"
+      ],
+      answer: "Binärdaten vor",
+      subjectname: "Wirtschaftsinformatik"
+    },
+    {
+      _id: "5a",
+      question: "test1 In welcher Phase findet die Migration von Daten statt?",
+      options: [
+        "In der Phase Abschaltung",
+        "In der Phase Integration",
+        "In der Phase Betrieb",
+        "In der Phase Erstellung / Entwicklung"
+      ],
+      answer: "In der Phase Abschaltung",
+      subjectname: "Wirtschaftsinformatik"
+    },
+    {
+      _id: "6a",
+      question: "test2 Digitale Güter liegen in Form von ...",
+      options: [
+        "Binärdaten vor",
+        "Computerdaten vor",
+        "elektronischen daten vor",
+        "großen Datenmengen vor"
+      ],
+      answer: "Binärdaten vor",
+      subjectname: "Wirtschaftsinformatik"
+    },
+    {
+      _id: "7a",
+      question: "test2 In welcher Phase findet die Migration von Daten statt?",
+      options: [
+        "In der Phase Abschaltung",
+        "In der Phase Integration",
+        "In der Phase Betrieb",
+        "In der Phase Erstellung / Entwicklung"
+      ],
+      answer: "In der Phase Abschaltung",
+      subjectname: "Wirtschaftsinformatik"
+    },
+    {
+      _id: "8a",
+      question: "Unter dem Begriff On-Demand versteht man, dass …",
+      options: [
+        "die zur Verfügungstellung innerhalb eines Tages erfolgen muss.",
+        "ein Service zu jeder Zeit, wenn man ihn benutzen möchte, in Echtzeit zur Verfügung steht.",
+        "die zur Verfügungstellung innerhalb von zwei Stunden erfolgen muss.",
+        "die zur Verfügungstellung innerhalb einer Stunde erfolgen muss"
+      ],
+      answer: "ein Service zu jeder Zeit, wenn man ihn benutzen möchte, in Echtzeit zur Verfügung steht.",
+      subjectname: "Wirtschaftsinformatik"
+    },
+    {
+      _id: "9a",
+      question: "test3 Digitale Güter liegen in Form von ...",
+      options: [
+        "Binärdaten vor",
+        "Computerdaten vor",
+        "elektronischen daten vor",
+        "großen Datenmengen vor"
+      ],
+      answer: "Binärdaten vor",
+      subjectname: "Wirtschaftsinformatik"
+    },
+    {
+      _id: "10a",
+      question: "test4 Digitale Güter liegen in Form von ...",
+      options: [
+        "Binärdaten vor",
+        "Computerdaten vor",
+        "elektronischen daten vor",
+        "großen Datenmengen vor"
+      ],
+      answer: "Binärdaten vor",
+      subjectname: "Wirtschaftsinformatik"
+    },
+    {
+      _id: "1b",
+      question: "Welcher der folgenden Faktoren ist ein Kano-Typ?",
+      options: [
+        "Rechtliche Relevanz",
+        "Randbedingung",
+        "Selektiver Faktor",
+        "Leistungsfaktor"
+      ],
+      answer: "Leistungsfaktor",
+      subjectname: "Requirements Engineering"
+    },
+    {
+      _id: "2b",
+      question: "Welche der folgenden Aussagen zur Ablauforganisation ist richtig?",
+      options: [
+        "Sie bildet die Organisationsstruktur einer IT-Landschaft.",
+        "Sie besteht immer aus mehreren Organisationseinheiten.",
+        "Sie bildet die Organisationsstruktur eines Unternehmens.",
+        "Sie steht im Abhängigkeitsverhältnis zur Aufbauorganisation."
+      ],
+      answer: "Sie bildet die Organisationsstruktur eines Unternehmens.",
+      subjectname: "Requirements Engineering"
+    },
+    {
+      _id: "3b",
+      question: "Der Inhalt welches der folgenden Elemente von Dokumentenstrukturen für Anforderungsdokumente eignet sich für die abschließende Prüfung, ob Inhalt und Form noch zu Zweck und Zielgruppe des Projekts passen?",
+      options: [
+        "Überblicksebene",
+        "Projektvision",
+        "Referenzen",
+        "Glossar"
+      ],
+      answer: "Projektvision",
+      subjectname: "Requirements Engineering"
+    },
+    {
+      _id: "1c",
+      question: "Wie nennt man ein regelmäßiges Zusammentreffen der Teammitglieder zur gemeinsamen Reflexion der Art und Qualität der Zusammenarbeit?",
+      options: [
+        "Jour fixe",
+        "Postmortem",
+        "Retrospektive",
+        "Workshop"
+      ],
+      answer: "Retrospektive",
+      subjectname: "Projektmanagement"
+    },
+    {
+      _id: "2c",
+      question: "Was ist keine Kategorie in der Risiko-Wert-Matrix?",
+      options: [
+        "Als zweites umsetzen",
+        "Als erstes umsetzen",
+        "Vermeiden",
+        "Im nächsten Release umset"
+      ],
+      answer: "Im nächsten Release umset",
+      subjectname: "Projektmanagement"
+    },
+    {
+      _id: "3c",
+      question: "test Unter dem Begriff On-Demand versteht man, dass …",
+      options: [
+        "die zur Verfügungstellung innerhalb eines Tages erfolgen muss.",
+        "ein Service zu jeder Zeit, wenn man ihn benutzen möchte, in Echtzeit zur Verfügung steht.",
+        "die zur Verfügungstellung innerhalb von zwei Stunden erfolgen muss.",
+        "die zur Verfügungstellung innerhalb einer Stunde erfolgen muss"
+      ],
+      answer: "ein Service zu jeder Zeit, wenn man ihn benutzen möchte, in Echtzeit zur Verfügung steht.",
+      subjectname: "Projektmanagement"
+    }
+  ];
+  
+  export default dataQuestions;
