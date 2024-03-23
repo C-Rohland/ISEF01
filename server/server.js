a61f5c6d7d3969f9109a1687442b4be8828107ce
crohland <christine.rohland@iu-study.org>
2024-03-23 14:05:55 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -9,22 +9,11 @@ import data from './database/data.js';
 config();
 
 const app = express();
+
+
 console.log(process.env.REACT_APP_SERVER_HOSTNAME); // Sollte die Server-URL ausgeben
 
-
-
-// // Konfiguriere CORS-Middleware, um dynamisch zu prüfen, ob die Anfrageherkunft erlaubt ist
-// app.use(cors({
-//     origin: ['https://isef-01.vercel.app', 'https://isef-01-ffntlkwgt-christines-projects-a764adc9.vercel.app', 'https://isef-01-jwu8q9u1k-christines-projects-a764adc9.vercel.app'],
-//     methods: ["GET", "POST"], // Füge weitere Methoden hinzu, die dein Frontend verwendet
-//     credentials: true // Falls deine Anfragen Credentials wie Cookies benötigen
-// }));
 app.use(cors());
-
-
-app.post('/login', (req, res) => {
-    res.json({ message: "Login-Endpoint erreicht" });
-});
 
 app.use(morgan('tiny'));
 app.use(express.json());
@@ -33,13 +22,10 @@ const port = process.env.PORT || 8080;
 
 app.use('/api', router);
 
-app.get('/login', (req, res) => {
-    try {
-        res.json("Get Request");
-    } catch (error) {
-        res.json(error);
-    }
+app.post('/login', (req, res) => {
+    res.json({ message: "Login-Endpoint erreicht" });
 });
+
 
 app.get('/api/questions', (req, res) => {
   res.json(data);
@@ -52,3 +38,6 @@ connect().then(() => {
 }).catch(error => {
     console.log("Invalid Database Connection", error);
 });
+
+
+
