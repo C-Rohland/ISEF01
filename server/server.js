19a432010cae3b50cf4bfc42c0a26721cc5977fc
crohland <christine.rohland@iu-study.org>
2024-03-23 13:55:51 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -9,15 +9,22 @@ import data from './database/data.js';
 config();
 
 const app = express();
+console.log(process.env.REACT_APP_SERVER_HOSTNAME); // Sollte die Server-URL ausgeben
 
 
-// Konfiguriere CORS-Middleware, um dynamisch zu prüfen, ob die Anfrageherkunft erlaubt ist
-app.use(cors({
-    origin: ['https://isef-01.vercel.app', 'https://isef-01-ffntlkwgt-christines-projects-a764adc9.vercel.app', 'https://isef-01-jwu8q9u1k-christines-projects-a764adc9.vercel.app'],
-    methods: ["GET", "POST"], // Füge weitere Methoden hinzu, die dein Frontend verwendet
-    credentials: true // Falls deine Anfragen Credentials wie Cookies benötigen
-}));
 
+// // Konfiguriere CORS-Middleware, um dynamisch zu prüfen, ob die Anfrageherkunft erlaubt ist
+// app.use(cors({
+//     origin: ['https://isef-01.vercel.app', 'https://isef-01-ffntlkwgt-christines-projects-a764adc9.vercel.app', 'https://isef-01-jwu8q9u1k-christines-projects-a764adc9.vercel.app'],
+//     methods: ["GET", "POST"], // Füge weitere Methoden hinzu, die dein Frontend verwendet
+//     credentials: true // Falls deine Anfragen Credentials wie Cookies benötigen
+// }));
+app.use(cors());
+
+
+app.post('/login', (req, res) => {
+    res.json({ message: "Login-Endpoint erreicht" });
+});
 
 app.use(morgan('tiny'));
 app.use(express.json());
