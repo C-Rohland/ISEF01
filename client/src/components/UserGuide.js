d48e46bd4527382ec980e7ab334aa1dfebe5f03e
Cristina Trebe <cristinatrebe@yahoo.com>
2024-03-26 14:22:32 +0100

    userGuide text-title darkblue

--- client/src/components/UserGuide.js
+++ client/src/components/UserGuide.js
@@ -24,53 +24,53 @@ function UserGuide() {
     </nav>
         </div>
              <div className="container">
-            <h3>Benutzerhandbuch für die Quiz-App</h3>
+            <h3 style={{ color: 'darkblue' }}>Benutzerhandbuch für die Quiz-App</h3>
         
             <section>
-                <h4>1. Anmelden:</h4>
+                <h4 style={{ color: 'darkblue' }}>1. Anmelden:</h4>
                 <p>
                 Um auf den QuizApp_Prototypen zugreifen zu können, musst du dich einloggen. Verwende dazu die folgenden Test-Anmeldedaten:
                 <br/>
-                E-Mail: <strong>iu.tester@iu.org </strong>
+                E-Mail: <strong style={{ color: 'darkblue' }}>iu.tester@iu.org </strong>
                 <br/>
-                Passwort: <strong>test123 </strong> 
+                Passwort: <strong style={{ color: 'darkblue' }}>test123 </strong> 
                 <br/>
                 Nach erfolgreicher Anmeldung hast du Zugriff auf die Quiz-Funktionen.
                 </p>
             </section>
             <section>
-                <h4>2. Quiz vorbereiten:</h4>
+                <h4 style={{ color: 'darkblue' }}>2. Quiz vorbereiten:</h4>
                 <p>
-                    Bevor du mit dem Quiz beginnst, empfehlen wir dir, die Quiz-Regeln zu lesen. 
-                    Die Quiz-Regeln erklären dir, wie das Quiz funktioniert und welche Regeln zu beachten sind. 
-                    Wähle dann das gewünschte Modul oder Thema aus, um deine Wissensgebiete festzulegen.
+                    Bevor du mit dem Quiz beginnst, empfehlen wir dir, die Quiz-Regeln zu lesen. <br/>
+                    Die Quiz-Regeln erklären dir, wie das Quiz funktioniert und welche Regeln zu beachten sind. <br/>
+                    Wähle dann das gewünschte Modul/Thema aus, um deine Wissensgebiete festzulegen. 
                 </p>
             </section>
             <section>
-                <h4>3. Quiz starten:</h4>
+                <h4 style={{ color: 'darkblue' }}>3. Quiz starten:</h4>
                 <p>
-                    Klicke auf "Quiz starten", um das Quiz zu beginnen.
-                    Das Quiz besteht aus 10 Fragen, die du beantworten musst.
+                    Klicke auf <strong style={{ color: 'darkblue' }}>Quiz starten</strong>, um das Quiz zu beginnen.<br/>
+                    Das Quiz besteht aus 10 Fragen, die du beantworten musst.<br/>
                     Stelle sicher, dass du bereit bist, bevor du das Quiz startest, da es keine Unterbrechung gibt, sobald das Quiz gestartet wurde.
                 </p>
             </section>
             <section>
-                <h4>4. Quiz-Ergebnisse:</h4>
+                <h4 style={{ color: 'darkblue' }}>4. Quiz-Ergebnisse:</h4>
                 <p>
-                    Nachdem du alle Fragen beantwortet hast, werden deine Quiz-Ergebnisse zusammengefasst und angezeigt.
-                    Du erhältst Informationen darüber, wie viele Fragen du richtig beantwortet hast und welche Punktzahl du erzielt hast
+                    Nachdem du alle Fragen beantwortet hast, werden deine Quiz-Ergebnisse zusammengefasst und angezeigt.<br/>
+                    Du erhältst Informationen darüber, wie viele Fragen du richtig beantwortet hast und welche Punktzahl du erzielt hast.<br/>
                     Du hast auch die Möglichkeit, das Quiz erneut zu starten, falls du weitere Übung benötigst.
                 </p>
             </section>
             <section>
-                <h4>5. Leaderboard:</h4>
+                <h4 style={{ color: 'darkblue' }}>5. Leaderboard:</h4>
                 <p>
-                    Auf der Leaderboard-Seite kannst du deine Ergebnisse mit denen anderer Studenten vergleichen.
+                    Auf der Leaderboard-Seite kannst du deine Ergebnisse mit denen anderer Studenten vergleichen.<br/>
                     Das Leaderboard zeigt die besten Quiz-Ergebnisse an und ermöglicht es dir, deine Leistung im Vergleich zu anderen zu sehen
                 </p>
             </section>
             <section>
-                <p>Viel Erfolg!</p>
+                <p><strong style={{ color: 'darkblue' }}>Viel Erfolg!</strong></p>
             </section>
 
         </div>
